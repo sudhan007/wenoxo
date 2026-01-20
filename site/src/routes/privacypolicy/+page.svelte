c28c416be4b995375a9024ff2bd4e7fedf2932da
sudhan007 <135029914+sudhan007@users.noreply.github.com>
2026-01-20 15:00:31 +0530

    Update +page.svelte

--- site/src/routes/privacypolicy/+page.svelte
+++ site/src/routes/privacypolicy/+page.svelte
@@ -1,55 +1,4 @@
-<html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>Privacy Policy</title>
-		<style>
-			body {
-				font-family: Arial, sans-serif;
-				margin: 0;
-				padding: 20px;
-				background-color: #f4f4f4;
-			}
-			.container {
-				max-width: 800px;
-				margin: 0 auto;
-				background-color: #fff;
-				padding: 20px;
-				box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-			}
-			h1,
-			h2,
-			h3 {
-				color: #4f80bc;
-			}
-			p {
-				color: #666;
-				line-height: 1.6;
-			}
-			.section {
-				margin-bottom: 40px;
-			}
-			.section-title {
-				font-size: 1.5em;
-				color: #4f80bc;
-				margin-bottom: 10px;
-			}
-			.section-content {
-				margin-left: 20px;
-			}
-			a {
-				color: #4f80bc;
-				text-decoration: none;
-			}
-			a:hover {
-				text-decoration: underline;
-			}
-			ul li {
-				margin-bottom: 10px;
-			}
-		</style>
-	</head>
-	<body>
+
 		<div class="container">
 			<div></div>
 			<p>Privacy Policy</p>
@@ -434,5 +383,49 @@
 				</li>
 			</ul>
 		</div>
-	</body>
-</html>
+		<style>
+			body {
+				font-family: Arial, sans-serif;
+				margin: 0;
+				padding: 20px;
+				background-color: #f4f4f4;
+			}
+			.container {
+				max-width: 800px;
+				margin: 0 auto;
+				background-color: #fff;
+				padding: 20px;
+				box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+			}
+			h1,
+			h2,
+			h3 {
+				color: #4f80bc;
+			}
+			p {
+				color: #666;
+				line-height: 1.6;
+			}
+			.section {
+				margin-bottom: 40px;
+			}
+			.section-title {
+				font-size: 1.5em;
+				color: #4f80bc;
+				margin-bottom: 10px;
+			}
+			.section-content {
+				margin-left: 20px;
+			}
+			a {
+				color: #4f80bc;
+				text-decoration: none;
+			}
+			a:hover {
+				text-decoration: underline;
+			}
+			ul li {
+				margin-bottom: 10px;
+			}
+		</style>
+
